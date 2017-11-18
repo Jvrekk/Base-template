e752d445b104f730f33ce34dc0543c156839180f
jvrek <jvreks@gmail.com>
2017-11-18 23:47:38 +0100

    back to previus version, too many issues with sass'ed bootstrap

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
 
 		</footer>
 	<?php wp_footer() ?>
+		</div>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <html>
 	<head>
-		<meta charset="UTF-8">
 		<title>Moje portfolio</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
 		<?php wp_head() ?>
 	</head>
 	
@@ -12,10 +14,46 @@ else:
 endif; ?>
 	
 	<body <?php body_class($body_theme_classes); ?> ></body>
-		<header>
+		<div class="container">
+			
+		
+			
 <?php if( get_header_image()): ?>
-			<img class="header-image" src="<?php header_image(); ?>" width="100%" />
+			<header>
+				<div class="row">
+					<img class="header-image" src="<?php header_image(); ?>" width="100%" />
+				</div>
+			</header>
 <?php endif; ?>
-		</header>
-		<?php wp_nav_menu(); ?>
+		
+			<div class="row">
+			 	<div class="col">
+					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					  <a class="navbar-brand" href="#">Navbar</a>
+					  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					  </button>
+					  <div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+						  <li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						  </li>
+						  <li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						  </li>
+						  <li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						  </li>
+						  <li class="nav-item">
+							<a class="nav-link disabled" href="#">Disabled</a>
+						  </li>
+						</ul>
+					  </div>
+					</nav>
+					<?php wp_nav_menu(); ?>
+				</div>
+			</div>
+		
+		
+	
 
